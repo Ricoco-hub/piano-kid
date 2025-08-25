7884893e517cf92dd58c6e4b29763eb7588edd24
Rise up <ronylyvan25@gmail.com>
2025-08-26 01:18:49 +0300

    Ajout du fichier tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        kids: ['Baloo 2', 'Nunito', 'Comic Sans MS', 'cursive', 'sans-serif'],
+      },
+      colors: {
+        primary: "#FFD700",
+        secondary: "#4F46E5"
+      }
+    }
+  },
+  plugins: [],
+}
